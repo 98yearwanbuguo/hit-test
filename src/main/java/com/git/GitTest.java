61b81b2631cdcd4671b65cca062119ac6cce85f9
lzh <1969636943@qq.com>
2022-08-09 08:56:51 +0800

    master commit1

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -3,12 +3,5 @@ package com.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test");
-        System.out.println("hello git");
-        System.out.println("hello git2");
-        System.out.println("hello git3");
-        System.out.println("master test");
-        System.out.println("hot-fix test");
-        System.out.println("ssh test");
-        System.out.println("pull test");
     }
 }
